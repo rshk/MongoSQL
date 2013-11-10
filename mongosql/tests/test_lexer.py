e33535705143efa5f52a29b6623dc87c1cde90ce
Samuele Santi <redshadow@hackzine.org>
2013-11-10 16:57:22 +0100

    Improved lexer test

--- mongosql/tests/test_lexer.py
+++ mongosql/tests/test_lexer.py
@@ -2,8 +2,37 @@ from mongosql.lexer import lexer
 
 
 def test_lexer_simple():
-    lexer.input('SELECT one, two FROM table WHERE field = "value";')
+    lexer.input("""
+    SELECT one, two FROM table
+    MATCH field == "value"
+    GROUP _id = name, count = sum(1);
+    """)
     data = list(lexer)
-    assert data[0].type == 'SELECT'
-    assert data[1].value == 'one'
-    pass
+    expected = [
+        ('SELECT', 'SELECT'),
+        ('SYMBOL', 'one'),
+        ('COMMA', ','),
+        ('SYMBOL', 'two'),
+        ('FROM', 'FROM'),
+        ('SYMBOL', 'table'),
+        ('MATCH', 'MATCH'),
+        ('SYMBOL', 'field'),
+        ('DBLEQUAL', '=='),
+        ('STRING', '"value"'),
+
+        ('GROUP', 'GROUP'),
+        ('SYMBOL', '_id'),
+        ('EQUAL', '='),
+        ('SYMBOL', 'name'),
+        ('COMMA', ','),
+        ('SYMBOL', 'count'),
+        ('EQUAL', '='),
+        ('SYMBOL', 'sum'),
+        ('LPAREN', '('),
+        ('INTEGER', 1),
+        ('RPAREN', ')'),
+        ('SEMICOLON', ';'),
+    ]
+    for i, (typ, val) in enumerate(expected):
+        assert data[i].type == typ
+        assert data[i].value == val
